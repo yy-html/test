90af53266c09642dea30690f9e92067e6bedf387
yy-html <1115390651@qq.com>
2019-08-21 01:01:04 +0800

    s

--- node.js
+++ node.js
@@ -1,5 +1,28 @@
 const process = require('process')
+const fs = require('fs')
+const log = console.log
 
-console.log(process)
+// process.nextTick(() => log(1))
 
+setTimeout(() => log(2))
+setImmediate(() => log(3))
+// setTimeout(() => log(4))
+
+
+// fs.readFile('./index.html', () => {
     
+// })
+
+
+
+// setTimeout(() => {
+
+//     setTimeout(() => {
+//         console.log('timeout')
+//     })
+//     setImmediate(() => {
+//         console.log('immediate')
+//     })
+//     process.nextTick(() => log(1))
+
+// })
