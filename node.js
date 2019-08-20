3553b0c8fa5cf85c11321975cf9e8bc0245f028c
yy-html <1115390651@qq.com>
2019-08-21 01:09:26 +0800

    s

--- node.js
+++ node.js
@@ -1,28 +1,13 @@
 const process = require('process')
 const fs = require('fs')
-const log = console.log
 
 // process.nextTick(() => log(1))
+// setTimeout(() => log(2))
+// setImmediate(() => log(3))
 
-setTimeout(() => log(2))
-setImmediate(() => log(3))
-// setTimeout(() => log(4))
+// node
+// supervisor nodemon cheerio
 
-
-// fs.readFile('./index.html', () => {
-    
-// })
-
-
-
-// setTimeout(() => {
-
-//     setTimeout(() => {
-//         console.log('timeout')
-//     })
-//     setImmediate(() => {
-//         console.log('immediate')
-//     })
-//     process.nextTick(() => log(1))
-
-// })
+// process.nextTick // 调用栈结束后执行（事件循环前）
+// setTimeout // timer 事件轮询poll后执行
+// setImmediate // check 事件轮询后执行（在主环境里 或先于timer或晚于timer 不确定）（在I/O操作的回调中 总是先于timer执行 晚于next tick队列）
